a87708c124d4d02b150fffa6fda970f0e372d03d
Javier Vera Zúñiga <jxvera@gmail.com>
2022-09-19 10:50:29 -0500

    Update mapu-inversion.py

--- mapu-inversion.py
+++ mapu-inversion.py
@@ -23,19 +23,25 @@ def sufijo_inversion(persona_sujeto,persona_objeto):
 
 ## diccionario de modo-persona-número
 
-indicativo_persona_inv = {}
+## primera + segunda
+indicativo_persona_inv_primera_segunda = {}
 ##
-indicativo_persona_inv['primera'] = {'tercera':{'singular':'ñ', 'dual':'yu', 'plural':'yiñ'}}
-indicativo_persona_inv['segunda'] = {'tercera':{'singular':'mi', 'dual':'mu', 'plural':'mün'}}
+indicativo_persona_inv_primera_segunda['primera'] = {'tercera':{'singular':'ñ', 'dual':'yu', 'plural':'yiñ'}}
+indicativo_persona_inv_primera_segunda['segunda'] = {'tercera':{'singular':'mi', 'dual':'mu', 'plural':'mün'}}
+## tercera
+indicativo_persona_inv_tercera = {}
 ##
-indicativo_persona_inv['tercera'] = {'primera':{'singular':'n', 'dual':'yu', 'plural':'iñ'},
-                                     'segunda':{'singular':'y', 'dual':'ymu', 'plural':'ymün'}}
+indicativo_persona_inv_tercera['primera'] = {'singular':'n', 'dual':'yu', 'plural':'iñ'}
+indicativo_persona_inv_tercera['segunda'] = {'singular':'y', 'dual':'ymu', 'plural':'ymün'}}
 
 ## morfemas de inversión
 
 def inversion(verbo, persona_sujeto, numero, polaridad, persona_objeto):
     ## ¿está correcto este orden? :O
-    return verbo + sufijo_polaridad(polaridad) + sufijo_inversion(persona_sujeto,persona_objeto)[0] + indicativo_persona_inv[persona_sujeto][persona_objeto][numero] + sufijo_inversion(persona_sujeto,persona_objeto)[1]
+    if (persona_sujeto == 'primera' or persona_sujeto == 'segunda') and persona_objeto == 'tercera':
+        return verbo + sufijo_polaridad(polaridad) + sufijo_inversion(persona_sujeto,persona_objeto)[0] + indicativo_persona_inv_primera_segunda[persona_sujeto][persona_objeto][numero] + sufijo_inversion(persona_sujeto,persona_objeto)[1]
+    if persona_sujeto == 'tercera' and (persona_objeto == 'primera' or persona_objeto == 'segunda'):
+        return verbo + sufijo_polaridad(polaridad) + sufijo_inversion(persona_sujeto,persona_objeto)[0] + indicativo_persona_inv_tercera[persona_objeto][numero] + sufijo_inversion(persona_sujeto,persona_objeto)[1]
 
 ## probamos todo para un verbo
 verbo = 'leli'#'leli' ## mirar
@@ -48,7 +54,7 @@ inversion(verbo, persona_sujeto, numero, polaridad, persona_objeto)
 
 verbo = st.text_input('verbo:', 'leli')
 persona_sujeto = st.selectbox('¿Cuál es la persona sujeto?', ['primera', 'segunda', 'tercera'], index=0)
-persona_objeto = st.selectbox('¿Cuál es la persona del objeto?', ['tercera'], index=0)
+persona_objeto = st.selectbox('¿Cuál es la persona del objeto?', ['primera','segunda','tercera'], index=0)
 numero = st.selectbox('¿Cuántos participan en el sujeto? (número)', ['singular', 'dual', 'plural'], index=0)
 polaridad = st.selectbox('polaridad', ['positiva','negativa'], index=1)
 
